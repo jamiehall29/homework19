71753c99693a424f6f0e800d586850d934394b33
Jamie Hall <jmhall498@gmail.com>
2022-10-21 23:31:29 -0400

    post iteration 19

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-console.log("please work I am begging you");
+console.log("please work I am literally begging you");
+
+
+
+$("nav li").hover(
+    function() {
+      $(this).addClass("hover");
+    }, function() {
+      $( this ).removeClass("hover");
+    }
+  );
+
+  $(".button").hover(
+    function() {
+      $(this).addClass("buttonHover");
+    }, function() {
+      $( this ).removeClass("buttonHover");
+    }
+  );
